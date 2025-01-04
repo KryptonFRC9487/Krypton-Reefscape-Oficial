3c61001d5b36cc06a0384ea407cfb7a9c40cab83
Guilherme Ferreira <guigododoti@gmail.com>
2025-01-04 16:44:46 -0300

    .
    .

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public final class Teste{
+
+    public static final int testes2 = 9;
+
+  }
+}
