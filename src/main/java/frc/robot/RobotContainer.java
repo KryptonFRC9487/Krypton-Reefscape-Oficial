59c51c23a12a83fd04ec5bcec10c2e5afbb22345
Nicollas Luz <nicollasgoias@hotmail.com>
2025-01-09 17:51:05 -0300

    add coisas swerve 2025

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,18 +4,116 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.Controle;
+import frc.robot.Constants.Trajetoria;
+import frc.robot.commands.MoveToPosition;
+import frc.robot.commands.SwerveCommand;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public class RobotContainer {
 
+  private SwerveSubsystem swerve = new SwerveSubsystem(
+    new File(Filesystem.getDeployDirectory(), "swerve")
+  );
+
+  private final XboxController xboxControle = new XboxController(
+    Controle.xboxControle
+  );
+
   public RobotContainer() {
+    NamedCommands.registerCommand("Intake", new PrintCommand("Intake"));
+
+    setDefaultCommands();
+    registerAutoCommands();
     configureBindings();
   }
 
+  private void setDefaultCommands() {
+     if (DriverStation.getAlliance().get() == DriverStation.Alliance.Blue) {
+      swerve.setDefaultCommand(
+      new SwerveCommand(
+        swerve,
+        () ->
+          -MathUtil.applyDeadband(xboxControle.getLeftY(), Controle.DEADBAND),
+        () ->
+          -MathUtil.applyDeadband(xboxControle.getLeftX(), Controle.DEADBAND),
+        () ->
+          -MathUtil.applyDeadband(xboxControle.getRightX(), Controle.DEADBAND),
+        () -> xboxControle.getRightBumperPressed()
+      )
+    );
+    } else {
+      swerve.setDefaultCommand(
+      new SwerveCommand(
+        swerve,
+        () ->
+          MathUtil.applyDeadband(xboxControle.getLeftY(), Controle.DEADBAND),
+        () ->
+          MathUtil.applyDeadband(xboxControle.getLeftX(), Controle.DEADBAND),
+        () ->
+          MathUtil.applyDeadband(xboxControle.getRightX(), Controle.DEADBAND),
+        () -> xboxControle.getRightBumperPressed()
+      )
+    );
+    }
+
+    if (DriverStation.getAlliance().get() == DriverStation.Alliance.Blue) {
+      new JoystickButton(xboxControle, XboxController.Button.kX.value)
+          .onTrue(new MoveToPosition(swerve, 1.630, 7.328));
+      new JoystickButton(xboxControle, XboxController.Button.kB.value)
+          .onTrue(new MoveToPosition(swerve, 0.707, 1.346));
+    } else if(DriverStation.getAlliance().get() == DriverStation.Alliance.Red) {
+      new JoystickButton(xboxControle, XboxController.Button.kB.value)
+          .onTrue(new MoveToPosition(swerve, 15.872, 7.364));
+      new JoystickButton(xboxControle, XboxController.Button.kX.value)
+          .onTrue(new MoveToPosition(swerve, 15.980, 0.758));
+    }
+
+  }
+
   private void configureBindings() {
+    new JoystickButton(xboxControle, XboxController.Button.kA.value)
+      .onTrue(new InstantCommand(
+        swerve::resetGyro
+    ));
+
+    new JoystickButton(xboxControle, XboxController.Button.kY.value)
+      .toggleOnTrue(Commands.startEnd(
+        swerve::disableHeading, swerve::resetHeading
+    ));
+
+    new JoystickButton(xboxControle, XboxController.Button.kRightBumper.value)
+    .onTrue(new MoveToPosition(swerve, 8, 4));
+  }
+
+  private void registerAutoCommands() {
+
   }
 
   public Command getAutonomousCommand() {
-    return null;
+    return swerve.getAutonomousCommand(Trajetoria.NOME_TRAJETORIA, true);
+  }
+
+  //Heading Correction 
+  public void setHeadingCorrection(boolean setHeadingCorrection){
+    swerve.swerveDrive.setHeadingCorrection(setHeadingCorrection);
+  }   
+
+  // Define os motores como coast ou brake
+  public void setMotorBrake(boolean brake) {
+    swerve.setMotorBrake(brake);
   }
 }
