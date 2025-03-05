eadbccc31239a17a685637be360ea784e43eb5ba
Miguel Ramos <miguel.ramos@inventsoftware.com.br>
2025-03-05 12:38:28 -0300

    correção paliativa

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -97,7 +97,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("Expelir Coral", m_outtakeSubsystem.setOuttakeSpeedCmd(0.3));
     NamedCommands.registerCommand("Parar Expelir", m_outtakeSubsystem.setOuttakeSpeedCmd(0));
 
-    NamedCommands.registerCommand("Score Coral - L4", m_scoreSystem.scoreL4CoralAuto());
+    NamedCommands.registerCommand("Score Coral - L4", m_scoreSystem.scoreCoral(ReefsScorePose.L4));
     NamedCommands.registerCommand("Elevator Init", m_scoreSystem.scoreCoral(ReefsScorePose.INITAL));
 
   }
